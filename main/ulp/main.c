53cdf16cf7ad12d8359d38669582ea6d1e265348
Kevin Sidwar <sidwarkd@hardlysoftware.com>
2025-01-11 05:55:12 +0000

    lesson 8 implementation

--- main/ulp/main.c
+++ main/ulp/main.c
@@ -31,6 +31,30 @@ void take_temperature_reading(void)
   // Set the state back to ready
   temp_reading.state = READY;
 }
+
+uint32_t nXOR(int n)
+{
+    // dummy here helps increase the size of the stack frame from 16 bytes
+    // to 32 bytes. It helps speed up the process of self destruction
+    int dummy[10];
+
+    // We want to track stack low water mark as our recursion deepens
+    cur_stack_address = getsp();
+    if(cur_stack_address < min_stack_address || min_stack_address == 0) {
+        min_stack_address = cur_stack_address;
+    }
+
+    // base condition to terminate the recursion when N = 0
+    if (n == 0) {
+        return 0;
+    }
+
+    // recursive case / recursive call
+    int res = n ^ nXOR(n - 1) ^ dummy[4];
+
+    return res;
+}
+
 int main (void)
 {
   loop_count++;
@@ -45,7 +69,9 @@ int main (void)
     take_temperature_reading();
   }
 
-  stackbuster(416);
+  nXOR(loop_count);
+
+  // stackbuster(416);
   // if(loop_count % 5 == 0){
   //   ulp_riscv_wakeup_main_processor();
   // }
